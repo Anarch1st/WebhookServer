a621ae4390dddc13fcb72b3702c43c932e3bc309
Anarch1st <saikat626@gmail.com>
2018-09-15 15:03:43 +0530

    Upgrading server

--- server/webhook.js
+++ server/webhook.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-const { execSync } = require('child_process');
+const server = require('./updateServer');
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -14,7 +14,7 @@ app.post('/webhook/post', function(req, res) {
 
 	const branch = tempArray[tempArray.length - 1];
 
-	if ((branch != 'master') && (branch != 'dev') ) {
+	if (branch != 'master') {
 		res.send({status : "success",
 				branch: branch,
 				updated: "No"});
@@ -25,20 +25,7 @@ app.post('/webhook/post', function(req, res) {
 			branch: branch,
 			updated: "Yes"});
 
-	execSync('pm2 stop app');
-	execSync('rm -rf ~/Documents/WaspServer');
-	execSync('git clone git@github.com:Anarch1st/WaspServer.git',
-	 {cwd: '/home/pi/Documents'});
-	execSync('git checkout '+branch,
-	 {cwd: '/home/pi/Documents/WaspServer'});
-	execSync('echo Installing dependencies');
-	execSync('npm install', {cwd: '/home/pi/Documents/WaspServer'});
-	execSync('echo Starting Server');
-	execSync('pm2 start server/app.js',
-	 {cwd: '/home/pi/Documents/WaspServer'});
-	execSync('echo saving config');
-	execSync('pm2 save');
-	execSync('echo done');
+	server.update(branch);
 
 });
 
