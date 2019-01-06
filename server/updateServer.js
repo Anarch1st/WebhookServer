98c271791478c6786e5bdfa14c740b41750144d2
saii <saikat626@gmail.com>
2019-01-06 19:30:46 +0530

    Updating architecture. All command execution done asynchronously to facilitate notify operations

--- server/updateServer.js
+++ server/updateServer.js
@@ -1,5 +1,5 @@
 const {
-  execSync
+  exec
 } = require('child_process');
 const fs = require('fs');
 const request = require('request');
@@ -26,90 +26,193 @@ function app(repo) {
   return repoDir(repo) + '/server/' + repo.toLowerCase() + '.js';
 }
 
-
 function update(repo, branch) {
-
   console.log("Repository: " + repo);
-  let repoNotifier = notifyZuk(repo);
-  var serverList = [];
   if (repo === "WaspServer") {
-    try {
-      var temp = execSync('pm2 jlist');
-      var json = JSON.parse(temp);
-      for (var ser of json) {
-        serverList.push(ser.name);
+    notify("Stopping all servers");
+    exec('pm2 stop all', (err, stdOut, stdErr) => {
+      if (err) {
+        console.error(err);
       }
-      repoNotifier("Stopping all servers");
-      execSync('pm2 stop all');
-    } catch (err) {}
+      copyPrivateToTemp(repo, branch)
+    })
   } else if (repo !== 'WebhookServer') {
-    serverList.push(repo.toLowerCase());
     try {
-      repoNotifier("Stopping " + repo);
-      execSync('pm2 stop ' + repo.toLowerCase());
-    } catch (err) {}
+      notify("Stopping " + repo);
+      exec('pm2 stop ' + repo.toLowerCase(), (err, stdOut, stdErr) => {
+        if (err) {
+          console.error(err);
+        }
+        copyPrivateToTemp(repo, branch)
+      });
+    } catch (err) {
+      console.error(err);
+      copyPrivateToTemp(repo, branch)
+    }
   }
-
-  try {
-    repoNotifier("Copying private to temp");
-    execSync('cp -R ' + privateDir(repo) + ' ' + tempDir(repo));
-  } catch (err) {}
-  try {
-    repoNotifier("Removing old Server");
-    execSync('rm -rf ' + repoDir(repo));
-  } catch (err) {}
-
-  repoNotifier("Cloning new server");
-  execSync('git clone git@github.com:Saii626/' + repo + '.git', {
-    cwd: serverDir
-  });
-  execSync('git checkout ' + branch, {
-    cwd: repoDir(repo)
-  });
-
-  repoNotifier("Installing server dependencies");
-  execSync('npm install', {
-    cwd: repoDir(repo)
-  });
-  try {
-    repoNotifier("Installing public dependencies")
-    execSync('npm install', {
-      cwd: publicDir(repo)
-    });
-  } catch (err) {}
-  try {
-    repoNotifier("Copying temp to private")
-    execSync('cp -R ' + tempDir(repo) + ' ' + privateDir(repo));
-  } catch (err) {}
-
-  if (repo === 'WebhookServer') {
-    execSync('pm2 save');
-    repoNotifier("Restarting WebHookServer");
-    execSync('pm2 restart webhookserver');
-  } else {
-    repoNotifier("Starting all servers");
-    execSync('pm2 start all');
-    execSync('pm2 save');
-  }
-
-  repoNotifier("Update Complete");
 }
 
+function copyPrivateToTemp(repo, branch) {
+  try {
+    notify("Copying private to temp");
+    exec('cp -R ' + privateDir(repo) + ' ' + tempDir(repo), (err, stdOut, stdErr) => {
+      if (err) {
+        console.error(err);
+      }
+      removeOldServer(repo, branch);
+    });
+  } catch (err) {
+    console.error(err);
+    removeOldServer(repo, branch);
+  }
+}
 
-function notifyZuk(repo) {
-  return (msg) => {
-    let postData = {
-      title: "WebHookServer updating " + repo,
-      body: msg
+function removeOldServer(repo, branch) {
+  try {
+    notify("Removing old Server");
+    exec('rm -rf ' + repoDir(repo), (err, stdOut, stdErr) => {
+      if (err) {
+        console.error(err);
+        notify("Unable to remove ");
+        return;
+      }
+      getNewServer(repo, branch);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function getNewServer(repo, branch) {
+  notify("Cloning new server");
+  exec('git clone git@github.com:Saii626/' + repo + '.git', {
+    cwd: serverDir
+  }, (err, stdOut, stdErr) => {
+    if (err) {
+      return;
     }
-    request.post('https://saikat.app/notify/zuk', {
-      form: postData
-    }, function(err, res, body) {
+
+    exec('git checkout ' + branch, {
+      cwd: repoDir(repo)
+    }, (err, stdOut, stdErr) => {
+      if (err) {
+        console.err(err);
+      }
+      installServerDependencies(repo, branch);
+    })
+  });
+}
+
+function installServerDependencies(repo, branch) {
+  notify("Installing server dependencies");
+  exec('npm install', {
+    cwd: repoDir(repo)
+  }, (err, stdOut, stdErr) => {
+    if (err) {
+      notify("Server dependency installation failed");
+      return;
+    }
+    installClientDependencies(repo, branch);
+  });
+}
+
+function installClientDependencies(repo, branch) {
+  try {
+    notify("Installing public dependencies")
+    exec('npm install', {
+      cwd: publicDir(repo)
+    }, (err, stdOut, stdErr) => {
+      if (err) {
+        console.error(err);
+      }
+      copyTempToPrivate(repo, branch);
+    });
+  } catch (err) {
+    copyTempToPrivate(repo, branch);
+  }
+}
+
+function copyTempToPrivate(repo, branch) {
+  try {
+    notify("Copying temp to private")
+    exec('cp -R ' + tempDir(repo) + ' ' + privateDir(repo), (err, stdOut, stdErr) => {
+      if (err) {
+        console.error(err);
+      }
+      startNewServer(repo, branch);
+    });
+  } catch (err) {
+    startNewServer(repo, branch);
+  }
+}
+
+function startNewServer(repo, branch) {
+  notify("Starting servers and saving config");
+  if (repo === 'WebhookServer') {
+    exec('pm2 save', (err, stdOut, stdErr) => {
+      if (err) {
+        console.error(err);
+      }
+
+      exec('pm2 restart webhookserver', (err, stdOut, stdErr) => {
+        if (err) {
+          notify("Unable to start WebhookServer")
+          console.error(err);
+          return;
+        }
+        notify("Server updated");
+      });
+    });
+  } else {
+    exec('pm2 start all', (err, stdOut, stdErr) => {
+      if (err) {
+        notify("Unable to start servers")
+      }
+
+      exec('pm2 save', (err, stdOut, stdErr) => {
+        if (err) {
+          console.error(err);
+        }
+        notify("Server updated");
+      })
+    });
+  }
+
+}
+
+function execute(cmd, options, cb, args) {
+  try {
+    execSync(cmd, options, (err, stdOut, stdErr) => {
       if (err) {
         console.error(err);
       }
     });
+  } catch (err) {
+    console.error(err);
   }
+}
+
+function notify(msg) {
+  let postData = {
+    title: "WebHookServer",
+    body: msg
+  }
+  request.post({
+    url: 'http://localhost:8020/zuk',
+    form: postData
+  }, function(err, res, body) {
+    if (err) {
+      console.error(err);
+    }
+  });
+  request.post({
+    url: 'http://localhost:8020/thinkpad',
+    form: postData
+  }, function(err, res, body) {
+    if (err) {
+      console.error(err);
+    }
+  });
 }
 
 module.exports.update = update;
