7077aa2f17408de4d6f110f13d00a32dd30bc4a7
saii <saikat626@gmail.com>
2019-01-06 18:57:24 +0530

    Sends serverupdate info to zuk

--- server/updateServer.js
+++ server/updateServer.js
@@ -1,82 +1,115 @@
-
-const { execSync } = require('child_process');
+const {
+  execSync
+} = require('child_process');
 const fs = require('fs');
+const request = require('request');
 
 const serverDir = '/home/pi/Servers';
+
 function repoDir(repo) {
-	return serverDir+'/'+repo;
+  return serverDir + '/' + repo;
 }
+
 function publicDir(repo) {
-	return repoDir(repo)+'/public';
+  return repoDir(repo) + '/public';
 }
+
 function privateDir(repo) {
-	return repoDir(repo)+'/private';
+  return repoDir(repo) + '/private';
 }
+
 function tempDir(repo) {
-	return serverDir+'/temp/'+repo;
+  return serverDir + '/temp/' + repo;
 }
+
 function app(repo) {
-	return repoDir(repo)+'/server/'+repo.toLowerCase()+'.js';
+  return repoDir(repo) + '/server/' + repo.toLowerCase() + '.js';
 }
 
 
 function update(repo, branch) {
 
-	console.log("Repository: "+repo);
-	var serverList = [];
-	if (repo === "WaspServer") {
-		try {
-			var temp = execSync('pm2 jlist');
-			var json = JSON.parse(temp);
-			for (var ser of json) {
-				serverList.push(ser.name);
-			}
-			execSync('pm2 stop all');
-		} catch (err) {}
-	} else if (repo !== 'WebhookServer') {
-		serverList.push(repo.toLowerCase());
-		try {
-			execSync('pm2 stop '+repo.toLowerCase());
-		} catch(err) {}
-	}
-	console.log("Server list: "+serverList);
+  console.log("Repository: " + repo);
+  let repoNotifier = notifyZuk(repo);
+  var serverList = [];
+  if (repo === "WaspServer") {
+    try {
+      var temp = execSync('pm2 jlist');
+      var json = JSON.parse(temp);
+      for (var ser of json) {
+        serverList.push(ser.name);
+      }
+      repoNotifier("Stopping all servers");
+      execSync('pm2 stop all');
+    } catch (err) {}
+  } else if (repo !== 'WebhookServer') {
+    serverList.push(repo.toLowerCase());
+    try {
+      repoNotifier("Stopping " + repo);
+      execSync('pm2 stop ' + repo.toLowerCase());
+    } catch (err) {}
+  }
 
-	try {
-		execSync('cp -R '+privateDir(repo)+' '+tempDir(repo));
-		console.log("Copy private folder to temp");
-	} catch (err) {}
-	try {
-		execSync('rm -rf '+repoDir(repo));
-		console.log("Remove private folder");
-	} catch (err) {}
+  try {
+    repoNotifier("Copying private to temp");
+    execSync('cp -R ' + privateDir(repo) + ' ' + tempDir(repo));
+  } catch (err) {}
+  try {
+    repoNotifier("Removing old Server");
+    execSync('rm -rf ' + repoDir(repo));
+  } catch (err) {}
 
-	console.log('Cloning');
-	execSync('git clone git@github.com:Saii626/'+repo+'.git', {cwd: serverDir});
-	console.log('Checking out');
-	execSync('git checkout '+branch, {cwd: repoDir(repo)});
-	console.log('Installing dependencies');
-	execSync('npm install', {cwd: repoDir(repo)});
-	try {
-		execSync('npm install', {cwd: publicDir(repo)});
-		console.log('Installing public dependencies');
-	} catch(err) {}
-	try {
-		execSync('cp -R '+tempDir(repo)+' '+privateDir(repo));
-		console.log("Copy temp private folder to private");
-	} catch(err) {}
+  repoNotifier("Cloning new server");
+  execSync('git clone git@github.com:Saii626/' + repo + '.git', {
+    cwd: serverDir
+  });
+  execSync('git checkout ' + branch, {
+    cwd: repoDir(repo)
+  });
 
-	if (repo === 'WebhookServer') {
-		execSync('pm2 save');
-		execSync('pm2 restart webhookserver');
-		console.log("Updated");
-	} else {
-		for (var server of serverList) {
-			console.log('Starting server: ' + repo);
-			execSync('pm2 start '+app(repo));
-		}
-		execSync('pm2 save');
-		console.log("Updated");
-	}
+  repoNotifier("Installing server dependencies");
+  execSync('npm install', {
+    cwd: repoDir(repo)
+  });
+  try {
+    repoNotifier("Installing public dependencies")
+    execSync('npm install', {
+      cwd: publicDir(repo)
+    });
+  } catch (err) {}
+  try {
+    repoNotifier("Copying temp to private")
+    execSync('cp -R ' + tempDir(repo) + ' ' + privateDir(repo));
+  } catch (err) {}
+
+  if (repo === 'WebhookServer') {
+    execSync('pm2 save');
+    repoNotifier("Restarting WebHookServer");
+    execSync('pm2 restart webhookserver');
+  } else {
+    repoNotifier("Starting all servers");
+    execSync('pm2 start all');
+    execSync('pm2 save');
+  }
+
+  repoNotifier("Update Complete");
+}
+
+
+function notifyZuk(repo) {
+  return (msg) => {
+    let postData = {
+      title: "WebHookServer updating " + repo,
+      body: msg
+    }
+    request.post('https://saikat.app/notify/zuk', {
+      form: postData
+    }, function(err, res, body) {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 }
 
 module.exports.update = update;
--- server/webhookserver.js
+++ server/webhookserver.js
@@ -6,36 +6,42 @@ const app = express();
 const httpServer = http.createServer(app);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.post('/webhook/post', function(req, res) {
-	const payload = req.body;
-	const name = payload.repository.name;
+  const payload = req.body;
+  const name = payload.repository.name;
 
-	var branch = null;
+  var branch = null;
 
-	if(payload.ref) {
-		var tempArray = payload.ref.split('/');
-		branch = tempArray[tempArray.length - 1];
-	} else {
-		branch = 'master';
-	}
+  if (payload.ref) {
+    var tempArray = payload.ref.split('/');
+    branch = tempArray[tempArray.length - 1];
+  } else {
+    branch = 'master';
+  }
 
-	if (branch != 'master') {
-		res.send({status : "success",
-				branch: branch,
-				updated: "No"});
-		return;
-	}
+  if (branch != 'master') {
+    res.send({
+      status: "success",
+      branch: branch,
+      updated: "No"
+    });
+    return;
+  }
 
-	res.send({status : "success",
-			branch: branch,
-			updated: "Yes"});
+  res.send({
+    status: "success",
+    branch: branch,
+    updated: "Yes"
+  });
 
-	server.update(name, branch);
+  server.update(name, branch);
 
 });
 
 httpServer.listen(process.env.PORT || 8010, function() {
-	console.log("WebHookServer started on port: "+httpServer.address().port);
+  console.log("WebHookServer started on port: " + httpServer.address().port);
 });
